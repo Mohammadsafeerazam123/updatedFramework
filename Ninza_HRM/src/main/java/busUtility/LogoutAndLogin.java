3bb0667ac13f5f0e8c55b6c1f1b224f20e9c4f11
acer <acer@LAPTOP-LMLVHQUV.mshome.net>
2026-09-21 16:21:46 +0530

    Changes done in LogoutAndLogin class

--- Ninza_HRM/src/main/java/busUtility/LogoutAndLogin.java
+++ Ninza_HRM/src/main/java/busUtility/LogoutAndLogin.java
@@ -3,6 +3,7 @@ package busUtility;
 public class LogoutAndLogin {
 		public static void main(String[] args) {
 			System.out.println("LOgout successful now delete the eclipse");
+			System.out.println("modification done");
 		}
 
 	}
